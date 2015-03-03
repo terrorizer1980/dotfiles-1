d838e45989cecadc553d5212277fd44e67ae4bbd
Nicholas Scheurich <nick@scheurich.me>
2015-03-03 17:16:16 -0600

    Add more informative watch task

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,8 @@ var gulp = require('gulp');
 var gutil = require('gulp-util');
 var coffee = require('gulp-coffee');
 
+var colors = gutil.colors
+
 gulp.task('coffee', function() {
   gulp.src('./.*.coffee')
     .pipe(coffee({bare: true}).on('error', gutil.log))
@@ -9,7 +11,11 @@ gulp.task('coffee', function() {
 });
 
 gulp.task('watch', ['coffee'], function() {
-  gulp.watch('./.*.coffee', ['coffee']);
+  var w = gulp.watch(['./*', './.*', './.*.coffee'], ['coffee']);
+  w.on('change', function(event) {
+    gutil.log('File', colors.cyan(event.path),
+              'was', colors.magenta( event.type));
+  });
 });
 
 gulp.task('default', ['coffee']);
