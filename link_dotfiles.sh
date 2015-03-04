a949f1e9a92480fdd89f79fc78ffcf8de8d3a6ea
Nicholas Scheurich <nick@scheurich.me>
2015-03-03 19:34:41 -0600

    Add linking script

---
+++ link_dotfiles.sh
@@ -0,0 +1,62 @@
+#!/bin/sh
+
+link ()
+{
+  orig="${HOME}/dotfiles/$1"
+  dest="${2:-${HOME}}/$1"
+  echo "Linking $orig to $dest"
+  ln -fs $orig $dest
+}
+
+# Set defaults
+d_vim="y"
+d_zsh="y"
+d_prezto="y"
+d_tmux="y"
+d_Slate="n"
+d_X="n"
+
+# Get user choices
+echo -n "Link Vim config? (Y/n) "
+read vim
+[ -z "$vim" ] && vim=$d_vim
+
+echo -n "Link Zsh config? (Y/n) "
+read zsh
+[ -z "$zsh" ] && zsh=$d_zsh
+
+echo -n "Link Prezto configs? (Y/n) "
+read prezto
+[ -z "$prezto" ] && prezto=$d_prezto
+
+echo -n "Link tmux config? (Y/n) "
+read tmux
+[ -z "$tmux" ] && tmux=$d_tmux
+
+echo -n "Link Slate config? (y/N) "
+read slate
+[ -z "$slate" ] && slate=$d_slate
+
+echo -n "Link X configs? (y/N) "
+read x
+[ -z "$x" ] && x=$d_x
+
+# Link the chosen files
+[ "$vim" == "y" ] && link ".vimrc"
+
+[ "$zsh" == "y" ] && link ".zshrc"
+
+if [ "$prezto" == "y" ]; then
+  link ".zpreztorc"
+  link "prompt_ngscheurich_setup" "${HOME}/.zprezto/modules/prompt/functions" 
+fi
+
+[ "$tmux" == "y" ] && link ".tmux.conf"
+
+[ "$slate" == "y" ] && link ".slate.js"
+
+if [ "$x" == "y" ]; then
+  link ".xinitrc"
+  link ".Xresources"
+fi
+
