207a63b10e7ddea6e0086d3ae33622e5fec86ba6
Nicholas Scheurich <nick@scheurich.me>
2015-05-18 12:21:23 -0400

    Make link script work properly

--- link_dotfiles.sh
+++ link_dotfiles.sh
@@ -1,11 +1,12 @@
-#!/bin/zsh
+#!/bin/bash
 
+# Script linking function
 link ()
 {
   orig="${HOME}/dotfiles/$1"
   dest="${2:-${HOME}}/$1"
-  echo "Linking $orig to $dest"
   ln -fs $orig $dest
+  echo "Linked $orig to $dest"
 }
 
 # Set defaults
@@ -25,29 +26,29 @@ read vim
 echo -n "Link Zsh config? (Y/n) "
 read zsh
 [ -z "$zsh" ] && zsh=$d_zsh
-
+ 
 echo -n "Link Prezto configs? (Y/n) "
 read prezto
 [ -z "$prezto" ] && prezto=$d_prezto
-
+ 
 echo -n "Link tmux config? (Y/n) "
 read tmux
 [ -z "$tmux" ] && tmux=$d_tmux
-
+ 
 echo -n "Link Slate config? (y/N) "
 read slate
 [ -z "$slate" ] && slate=$d_slate
-
+ 
 echo -n "Link X configs? (y/N) "
 read x
 [ -z "$x" ] && x=$d_x
-
+ 
 echo -n "Link Sublime Text configs? (y/N) "
 read sublime
 [ -z "$sublime" ] && sublime=$d_sublime
 
 # Link the chosen files
-[ "$vim" == "y" ] && link ".vimrc"
+[ "$vim" = "y" ] && link ".vimrc"
 
 [ "$zsh" == "y" ] && link ".zshrc"
 
@@ -55,7 +56,7 @@ if [ "$prezto" == "y" ]; then
   link ".zpreztorc"
   link "prompt_ngscheurich_setup" "${HOME}/.zprezto/modules/prompt/functions" 
 fi
-
+ 
 [ "$tmux" == "y" ] && link ".tmux.conf"
 
 [ "$slate" == "y" ] && link ".slate.js"
@@ -64,8 +65,8 @@ if [ "$x" == "y" ]; then
   link ".xinitrc"
   link ".Xresources"
 fi
-
+ 
 if [ "$sublime" == "y" ]; then
   link "Preferences.sublime-settings" "${HOME}/Library/Application Support/Sublime Text 3/Packages/User"
 fi
-
+ 
