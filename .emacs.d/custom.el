d7e4e34a10fdb8a6b3e9a30e36691d9d996d67cd
Nicholas Scheurich <nick@scheurich.me>
2016-05-27 12:33:35 -0500

    Style org-mode

--- .emacs.d/custom.el
+++ .emacs.d/custom.el
@@ -10,4 +10,9 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(fringe ((t (:background "#181818")))))
+ '(fringe ((t (:background "#181818"))))
+ '(org-document-title ((t (:foreground "#dc9656" :weight bold :height 1.625))))
+ '(org-level-1 ((t (:inherit outline-1 :weight bold :height 1.5))))
+ '(org-level-2 ((t (:inherit outline-2 :weight bold :height 1.625))))
+ '(org-level-3 ((t (:inherit outline-3 :weight bold :height 1.375))))
+ '(org-quote ((t (:inherit org-block :slant italic :height 1.25)))))
