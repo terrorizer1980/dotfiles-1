97466dcb34e83748f2a002b286da716df1b7ee1e
Nicholas Scheurich <nick@scheurich.me>
2021-04-22 20:30:54 -0500

    Add Xa command to catch my fuck ups

--- nvim/.config/nvim/lua/ngs/commands.lua
+++ nvim/.config/nvim/lua/ngs/commands.lua
@@ -9,3 +9,5 @@ vim.cmd [[command! PackerUpdate  lua require("ngs.packages").update()]]
 vim.cmd [[command! PackerClean   lua require("ngs.packages").clean()]]
 vim.cmd [[command! PackerSync    lua require("ngs.packages").sync()]]
 vim.cmd [[command! PackerGet     lua require("ngs.util").get_packer()]]
+
+vim.cmd [[command! Xa <Cmd>xa<CR>]]
