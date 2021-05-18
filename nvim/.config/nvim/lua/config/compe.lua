1a3cfe71defa8291b2fd33486e355bcc8909f968
Nicholas Scheurich <nick@scheurich.me>
2021-05-18 18:19:34 -0500

    Improve <Tab>/<S-Tab> behavior

--- nvim/.config/nvim/lua/config/compe.lua
+++ nvim/.config/nvim/lua/config/compe.lua
@@ -1,5 +1,5 @@
 local map = require("ngs.util").map
-local fn, api = vim.fn, vim.api
+local fn = vim.fn
 
 require("compe").setup({
   min_length = 1,
@@ -13,55 +13,37 @@ require("compe").setup({
   },
 })
 
-map("i", "<CR>",    "compe#confirm('<CR>')", {silent = true, expr = true})
-map("i", "<C-l>",   "vsnip#available(1) ? '<Plug>(vsnip-expand-or-jump)' : '<C-l>'", {expr = true, noremap = false})
-map("s", "<C-l>",   "vsnip#available(1) ? '<Plug>(vsnip-expand-or-jump)' : '<C-l>'", {expr = true, noremap = false})
-map("i", "<Tab>",   [[luaeval('require("config.compe").tab(1)')]], {silent = true, expr = true, noremap = false})
-map("i", "<S-Tab>", [[luaeval('require("config.compe").tab(-1)')]], {silent = true, expr = true, noremap = false})
-
-local function replace_termcodes(str)
-  return api.nvim_replace_termcodes(str, true, true, true)
+local function send(str)
+  return vim.api.nvim_replace_termcodes(str, true, true, true)
 end
 
-local function check_backspace()
-  local col = fn.col(".") - 1
-  if col == 0 or fn.getline("."):sub(col, col):match("%s") then
-    return true
+local function tab()
+  if fn.pumvisible() == 1 then
+    return send("<C-n>")
+  elseif fn.call("vsnip#available", {1}) == 1 then
+    return send("<Plug>(vsnip-expand-or-jump)")
   else
-    return false
+    return send("<Tab>")
   end
 end
 
-local function prev_next(dir)
-  if dir == 1 then
-    return replace_termcodes("<C-n>")
-  elseif dir == -1 then
-    return replace_termcodes("<C-p>")
+local function s_tab()
+  if fn.pumvisible() == 1 then
+    return send("<C-p>")
+  elseif fn.call("vsnip#jumpable", {-1}) == 1 then
+    return send("<Plug>(vsnip-jump-prev)")
+  else
+    return send("<S-Tab>")
   end
 end
+
+map("i", "<CR>",    "compe#confirm('<CR>')", {expr = true})
+map("i", "<Tab>",   [[luaeval('require("config.compe").tab()')]], {expr = true, noremap = false})
+map("s", "<Tab>",   [[luaeval('require("config.compe").tab()')]], {expr = true, noremap = false})
+map("i", "<S-Tab>", [[luaeval('require("config.compe").s_tab()')]], {expr = true, noremap = false})
+map("s", "<S-Tab>", [[luaeval('require("config.compe").s_tab()')]], {expr = true, noremap = false})
 
 return {
-  tab = function (dir)
-    if fn.pumvisible() == 1 then
-      return prev_next(dir)
-    elseif fn.call("vsnip#available", {1}) == 1 then
-      return replace_termcodes("<Plug>(vsnip-expand-or-jump)")
-    elseif fn.call("vsnip#jumpable", {-1}) == 1 then
-      return replace_termcodes("<Plug>(vsnip-jump-prev)")
-    elseif check_backspace() and dir == 1 then
-      return replace_termcodes("<Tab>")
-    elseif dir == 1 then
-      return fn["compe#complete"]()
-    else
-      return replace_termcodes("<S-Tab>")
-    end
-  end,
-
-  cr = function ()
-    if fn["vsnip#expandable"]() then
-      return replace_termcodes("<Plug>(vsnip-expand)")
-    else
-      return replace_termcodes("<CR>")
-    end
-  end
+  tab = tab,
+  s_tab = s_tab,
 }
