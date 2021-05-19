ff520ef37be2056be92761356d9879c95d17567a
Nicholas Scheurich <nick@scheurich.me>
2021-05-19 01:27:56 -0500

    Set up additional formatters

--- nvim/.config/nvim/lua/config/formatter.lua
+++ nvim/.config/nvim/lua/config/formatter.lua
@@ -1,38 +1,74 @@
-local config = {
-  mix_format =
-    function ()
-      return {
-        exe = 'mix',
-        args = {'format'},
+local function mix()
+    return {
+        exe = "mix",
+        args = {"format"},
         stdin = false,
-        tempfile_dir = "/tmp",
-      }
-    end,
+        tempfile_dir = "/tmp"
+    }
+end
 
-  prettier =
-    function ()
-      return {
+local function rustfmt()
+    return {exe = "rustfmt", args = {"--emit=stdout"}, stdin = true}
+end
+
+local function prettier()
+    return {
         exe = "prettier",
         args = {"--stdin-filepath", vim.api.nvim_buf_get_name(0)},
         stdin = true
-      }
-    end
-}
+    }
+end
 
-require('formatter').setup({
-  quiet = true,
+local function luafmt()
+    return {
+        exe = "lua-format",
+        args = {"--", vim.api.nvim_buf_get_name(0)},
+        stdin = true
+    }
+end
 
-  filetype = {
-    elixir = {config.mix_format},
-    javascript = {config.prettier},
-    typescript = {config.prettier},
-    typescriptreact = {config.prettier},
-  }
+local function shfmt()
+    return {
+        exe = "shfmt",
+        args = {"-s", "-w", "--", vim.api.nvim_buf_get_name(0)},
+        stdin = true
+    }
+end
+
+local function dockfmt()
+    return {
+        exe = "dockfmt",
+        args = {"--write", "--", vim.api.nvim_buf_get_name(0)},
+        stdin = true
+    }
+end
+
+require("formatter").setup({
+    -- quiet = true,
+    logging = true,
+
+    filetype = {
+        dockerfile = {dockfmt},
+        json = {prettier},
+        lua = {luafmt},
+        rust = {rustfmt},
+        toml = {prettier},
+        yaml = {prettier},
+        css = {prettier},
+        elixir = {mix},
+        html = {prettier},
+        javascript = {prettier},
+        markdown = {prettier},
+        sh = {shfmt},
+        typescript = {prettier},
+        typescriptreact = {prettier},
+        zsh = {shfmt}
+    }
 })
 
 vim.api.nvim_exec([[
 augroup format
   autocmd!
-  autocmd BufWritePost *.ex,*.exs,*.js,*.ts,*.tsx FormatWrite
+  autocmd BufWritePost *.ex,*.exs,*.js,*.ts,*.tsx,*.yaml,*.toml,*.css,*.html,*.md FormatWrite
 augroup END
 ]], true)
