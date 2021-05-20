188e2db5da455c3e38f4d9958ed45bddda6ba6dc
Nicholas Scheurich <nick@scheurich.me>
2021-05-20 01:54:13 -0500

    Formatting

--- nvim/.config/nvim/lua/config/formatter.lua
+++ nvim/.config/nvim/lua/config/formatter.lua
@@ -1,74 +1,91 @@
-local function mix()
-    return {
-        exe = "mix",
-        args = {"format"},
-        stdin = false,
-        tempfile_dir = "/tmp"
-    }
-end
+local util = require("ngs.util")
 
-local function rustfmt()
-    return {exe = "rustfmt", args = {"--emit=stdout"}, stdin = true}
+local function mix_format()
+  return {
+    exe   = "mix",
+    args  = {"format"},
+    stdin = false,
+    tempfile_dir = "/tmp"
+  }
 end
 
 local function prettier()
-    return {
-        exe = "prettier",
-        args = {"--stdin-filepath", vim.api.nvim_buf_get_name(0)},
-        stdin = true
-    }
+  return {
+    exe   = "prettier",
+    args  = {"--stdin-filepath", vim.api.nvim_buf_get_name(0)},
+    stdin = true
+  }
 end
 
 local function luafmt()
-    return {
-        exe = "lua-format",
-        args = {"--", vim.api.nvim_buf_get_name(0)},
-        stdin = true
-    }
+  return {
+    exe   = "lua-format",
+    args  = {"--", vim.api.nvim_buf_get_name(0)},
+    stdin = true
+  }
+end
+
+local function rustfmt()
+  return {
+    exe   = "rustfmt",
+    args  = {"--emit=stdout"},
+    stdin = true
+  }
 end
 
 local function shfmt()
-    return {
-        exe = "shfmt",
-        args = {"-s", "-w", "--", vim.api.nvim_buf_get_name(0)},
-        stdin = true
-    }
+  return {
+    exe   = "shfmt",
+    args  = {"-s", "-w", "--", vim.api.nvim_buf_get_name(0)},
+    stdin = true
+  }
 end
 
 local function dockfmt()
-    return {
-        exe = "dockfmt",
-        args = {"--write", "--", vim.api.nvim_buf_get_name(0)},
-        stdin = true
-    }
+  return {
+    exe   = "dockfmt",
+    args  = {"--write", "--", vim.api.nvim_buf_get_name(0)},
+    stdin = true
+  }
 end
 
 require("formatter").setup({
-    -- quiet = true,
-    logging = true,
+  -- quiet = true,
+  logging = true,
 
-    filetype = {
-        dockerfile = {dockfmt},
-        json = {prettier},
-        lua = {luafmt},
-        rust = {rustfmt},
-        toml = {prettier},
-        yaml = {prettier},
-        css = {prettier},
-        elixir = {mix},
-        html = {prettier},
-        javascript = {prettier},
-        markdown = {prettier},
-        sh = {shfmt},
-        typescript = {prettier},
-        typescriptreact = {prettier},
-        zsh = {shfmt}
-    }
+  filetype = {
+    dockerfile      = {dockfmt},
+    json            = {prettier},
+    lua             = {luafmt},
+    rust            = {rustfmt},
+    toml            = {prettier},
+    yaml            = {prettier},
+    css             = {prettier},
+    elixir          = {mix_format},
+    html            = {prettier},
+    javascript      = {prettier},
+    markdown        = {prettier},
+    sh              = {shfmt},
+    typescript      = {prettier},
+    typescriptreact = {prettier},
+    zsh             = {shfmt}
+  }
 })
 
-vim.api.nvim_exec([[
-augroup format
-  autocmd!
-  autocmd BufWritePost *.ex,*.exs,*.js,*.ts,*.tsx,*.yaml,*.toml,*.css,*.html,*.md FormatWrite
-augroup END
-]], true)
+local exts = {
+  "css",
+  "ex",
+  "exs",
+  "html",
+  "js",
+  "markdown",
+  "md",
+  "toml",
+  "ts",
+  "tsx",
+  "yaml",
+}
+exts = vim.tbl_map(function (x) return "*." .. x end, exts)
+exts = table.concat(exts, ",")
+
+util.augroup("format", {"BufWritePost " .. exts .. " FormatWrite"})
