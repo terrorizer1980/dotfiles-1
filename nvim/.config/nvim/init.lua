04bebef2c5a44ff4c9c6101a83bc7d63045d601b
Nicholas Scheurich <nick@scheurich.me>
2021-05-20 00:08:10 -0500

    Formatting

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -13,20 +13,20 @@
 -- Repo: https://github.com/ngscheurich/dotfiles
 
 -- Disable some built-in plugins
--- vim.g.loaded_python_provider  = 0
--- vim.g.loaded_python3_provider = 0
+vim.g.loaded_python_provider  = 0
+vim.g.loaded_python3_provider = 0
 vim.g.loaded_ruby_provider    = 0
 vim.g.loaded_node_provider    = 0
 vim.g.loaded_perl_provider    = 0
 
 --- Package management
-require('ngs.packages')
+require("ngs.packages")
 
 -- Package configuration
-require('config.compe')
-require('config.devicons')
-require('config.formatter')
-require('config.lspconfig')
-require('config.lspkind')
-require('config.startify')
-require('config.treesitter')
+require("config.compe")
+require("config.devicons")
+require("config.formatter")
+require("config.lspconfig")
+require("config.lspkind")
+require("config.startify")
+require("config.treesitter")
