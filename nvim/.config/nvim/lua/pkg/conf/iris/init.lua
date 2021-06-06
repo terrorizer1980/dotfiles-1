b8cdf138fc3f413053ff6aa9244d75315af23922
Nicholas Scheurich <nick@scheurich.me>
2021-06-06 01:43:54 -0500

    Add iris config

---
+++ nvim/.config/nvim/lua/pkg/conf/iris/init.lua
@@ -0,0 +1,11 @@
+require("iris").setup({
+    palettes = {
+        tokyonight = require("pkg.conf.iris.tokyonight")
+    },
+
+    callbacks = {
+        require("pkg.conf.iris.nvim"),
+        require("pkg.conf.iris.tmux"),
+        require("pkg.conf.iris.kitty"),
+    }
+})
---
+++ nvim/.config/nvim/lua/pkg/conf/iris/tokyonight.lua
@@ -0,0 +1,29 @@
+local exists, module = pcall(require, "tokyonight.colors")
+if not exists then return end
+
+local blend = require("iris.lib.blend").blend
+local colors = module.setup()
+
+return {
+  base00     = colors.bg,
+  base01     = colors.bg_dark,
+  base02     = colors.bg_highlight,
+  base03     = colors.bg_highlight,
+  base04     = colors.fg_gutter,
+  base05     = colors.comment,
+  base06     = colors.fg_dark,
+  base07     = colors.fg,
+  base08     = colors.red,
+  base09     = colors.orange,
+  base0A     = colors.yellow,
+  base0B     = colors.teal,
+  base0C     = colors.cyan,
+  base0D     = colors.blue,
+  base0E     = colors.magenta,
+  base0F     = colors.git.delete,
+  gutter     = blend(colors.bg, colors.fg_gutter, 0.85),
+  cursorline = blend(colors.bg, colors.fg_gutter, 0.75),
+  add        = colors.gitSigns.add,
+  change     = colors.gitSigns.change,
+  delete     = colors.gitSigns.delete,
+}
