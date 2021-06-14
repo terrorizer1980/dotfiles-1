ed6191170822c8426c3ab2dc2cd7363d76c9f0ef
Nicholas Scheurich <nick@scheurich.me>
2021-06-13 19:25:51 -0500

    Implement Clubhouse branch shorthand

--- nvim/.config/nvim/lua/pkg/conf/galaxyline/git.lua
+++ nvim/.config/nvim/lua/pkg/conf/galaxyline/git.lua
@@ -1,19 +1,31 @@
 local condition = require("galaxyline.condition")
-local vcs = require("galaxyline.provider_vcs")
 local colors = require("iris.palette").get_color_fn
+
+local function ch_shorten(branch)
+  return string.match(branch, [[^.+/ch%d+/(.*)]])
+end
 
 return {
   {
     GitIcon = {
       provider = function() return "  " end,
       highlight = {"NONE", colors("line_base")},
-    },
+    }
   },
   {
     GitBranch = {
-      provider = function() return vcs.get_git_branch() .. " " end,
+      provider = function()
+        if not vim.fn.exists("g:loaded_fugitive") then
+          return ""
+        end
+        local head = vim.fn["fugitive#Head"]()
+        if head then
+          local o = ch_shorten(head) or head
+          return o.." "
+        end
+      end,
       condition = condition.check_git_workspace,
       highlight = {colors("white"), colors("line_base")},
-    },
-  },
+    }
+  }
 }
