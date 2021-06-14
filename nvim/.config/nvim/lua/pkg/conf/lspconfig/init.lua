3173a9c61569a949808831bb3033cb2c05d9c17d
Nicholas Scheurich <nick@scheurich.me>
2021-06-13 19:25:14 -0500

    Add leader mappings

--- nvim/.config/nvim/lua/pkg/conf/lspconfig/init.lua
+++ nvim/.config/nvim/lua/pkg/conf/lspconfig/init.lua
@@ -4,37 +4,39 @@ local util = require("ngs.util")
 
 -- Callback for when LSP client attaches to buffer
 local function on_attach(client, bufnr)
-    local function map(lhs, rhs)
-        util.bufmap(bufnr, "n", lhs, rhs, {silent = true})
-    end
+  local function map(lhs, rhs)
+    util.bufmap(bufnr, "n", lhs, rhs, {silent = true})
+  end
 
-    vim.cmd("redir @q")
-    vim.cmd("redir END")
+  map("<C-]>", "<Cmd>lua vim.lsp.buf.definition()<CR>")
+  map("K",     "<Cmd>lua vim.lsp.buf.hover()<CR>")
+  map("gca",   "<Cmd>lua vim.lsp.buf.code_action()<CR>")
+  map("gd",    "<Cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<CR>")
+  map("gf",    "<Cmd>lua vim.lsp.buf.formatting_sync()<CR>")
+  map("gr",    "<Cmd>lua require('telescope.builtin').lsp_references()<CR>")
+  map("gs",    "<Cmd>lua require('telescope.builtin').lsp_document_symbols()<CR>")
+  map("gS",    "<Cmd>lua require('telescope.builtin').lsp_workspace_symbols()<CR>")
 
-    map("<C-]>", "<Cmd>lua vim.lsp.buf.definition()<CR>")
-    map("K", "<Cmd>lua vim.lsp.buf.hover()<CR>")
-    map("gca", "<Cmd>lua vim.lsp.buf.code_action()<CR>")
-    map("gd", "<Cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<CR>")
-    map("gf", "<Cmd>lua vim.lsp.buf.formatting_sync()<CR>")
-    map("gr", "<Cmd>lua require('telescope.builtin').lsp_references()<CR>")
-    map("gs", "<Cmd>lua require('telescope.builtin').lsp_document_symbols()<CR>")
-    map("gS",
-        "<Cmd>lua require('telescope.builtin').lsp_workspace_symbols()<CR>")
+  map("<Leader>ld", "<Cmd>lua vim.lsp.buf.definition()<CR>")
+  map("<Leader>lh", "<Cmd>lua vim.lsp.buf.hover()<CR>")
+  map("<Leader>la", "<Cmd>lua vim.lsp.buf.code_action()<CR>")
+  map("<Leader>lD", "<Cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<CR>")
+  map("<Leader>lf", "<Cmd>lua vim.lsp.buf.formatting_sync()<CR>")
+  map("<Leader>lr", "<Cmd>lua require('telescope.builtin').lsp_references()<CR>")
+  map("<Leader>ls", "<Cmd>lua require('telescope.builtin').lsp_document_symbols()<CR>")
+  map("<Leader>lS", "<Cmd>lua require('telescope.builtin').lsp_workspace_symbols()<CR>")
 
-    lsp_format.setup(client.config.filetypes)
+  lsp_format.setup(client.config.filetypes)
 end
 
 -- Set up servers with default settings
 local servers = {"vimls", "rls", "terraformls", "gdscript"}
 for _, server in ipairs(servers) do
-    lspconfig[server].setup({on_attach = on_attach})
+  lspconfig[server].setup({on_attach = on_attach})
 end
 
 -- Set up servers with custom settings
-servers = {
-    "efm", "elixirls", "haxe_language_server", "omnisharp", "sumneko_lua",
-    "tsserver"
-}
+servers = {"efm", "elixirls", "haxe_language_server", "omnisharp", "sumneko_lua", "tsserver"}
 for _, server in ipairs(servers) do
-    require("pkg.conf.lspconfig." .. server)(on_attach)
+  require("pkg.conf.lspconfig." .. server)(on_attach)
 end
